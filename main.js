36246d880be73ea204be5dcb0676d4046c53f89f
sn8ow <80784394+Sn8ow@users.noreply.github.com>
2021-06-11 13:00:58 +0200

    Update main.js

--- main.js
+++ main.js
@@ -117,7 +117,7 @@ function renderBox(x=0,y=0,z=0,w=1,h=5,c=new pc.Color(1, 1, 1)) {
 
 function createGui() {
     var gui = new guify({
-        title: 'BlockSploit',
+        title: 'ArktosSploit',
         align: 'right',
         theme: 'dark',
         opacity: 0.95,
